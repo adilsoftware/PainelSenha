f4400204d9c7e855cf4edba7de4cf4a13fd38233
Adilson da Silva Guilherme <74576838+adilsoftware@users.noreply.github.com>
2021-08-30 13:54:27 -0300

    Update conex.php

--- Connections/conex.php
+++ Connections/conex.php
@@ -1,8 +1,8 @@
 <?php
 $hostname_conex = "fdb30.awardspace.net";
-$database_conex = "3639130_adilsoftware";
-$username_conex = "3639130_adilsoftware";
-$password_conex = "ad81guilherme";
+$database_conex = "";
+$username_conex = "";
+$password_conex = "";
 // creación de la conexión a la base de datos con mysql_connect()
 $conex = mysqli_connect($hostname_conex, $username_conex, $password_conex, $database_conex) or 
 die ("No se ha podido conectar al servidor de Base de datos"); 
