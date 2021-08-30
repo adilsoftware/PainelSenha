97df223d58212d9bb25b46c6ca2bfd7ac0501701
Adilson da Silva Guilherme <74576838+adilsoftware@users.noreply.github.com>
2021-08-30 13:54:59 -0300

    Update nova.php

--- ajax/nova.php
+++ ajax/nova.php
@@ -2,9 +2,9 @@
 $nova = $_GET['valor1'];
 
 $servidor = "pdb41.awardspace.net";
-$usuario = "3639130_adilsoftware";
-$senha = "ad81guilherme";
-$dbname = "3639130_adilsoftware";
+$usuario = "";
+$senha = "";
+$dbname = "";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
